acb8e00f397f6ccb16ed6784a17f5012c2ae4868
Asilbek Soliyev <asilbeksoliyev@Air-Asilbek.home>
2022-08-26 20:04:56 +0500

    2task completed

--- 2 task/Program.cs
+++ 2 task/Program.cs
@@ -3,4 +3,34 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Write("Write 3 numbers, Writr First: ");
 
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write Second number: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write Third number: ");
+
+int number3 = Convert.ToInt32(Console.ReadLine());
+if(number1>number2)
+{
+    if(number1>number3)
+    {
+        Console.WriteLine("Max= " + number1);
+    }
+    else
+    {
+        Console.WriteLine("Max= " + number3);
+    }
+}
+else
+if(number2>number3)
+{
+    Console.WriteLine("Max= " + number2);
+}
+else
+{
+    Console.WriteLine("Max= " + number3);
+}
